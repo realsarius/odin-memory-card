606e57d59c61e3254e2a45d860b84e3cd498a30a
Berkan Sözer <berkansozer@protonmail.com>
2023-11-17 14:13:33 +0300

    initializing the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Score from "./components/Score";
+
+function App() {
+  return (
+    <>
+      <Score />
+    </>
+  );
+}
+
+export default App;
